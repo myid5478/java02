ae6c13d84d4480e553576d8529a28fd8657abcf4
myid5478 <myid5478@gmail.com>
2019-11-01 14:02:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] arg){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("請輸入出生年月日 ex:YYYY/MM/DD");
+        String s=sc.next();
+        System.out.print(s);
+
+
+
+
+    }
+
+}
